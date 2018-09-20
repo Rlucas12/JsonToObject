fe4814bff8f66a5ee85b3c1ecd26990934911c8d
REYNES Lucas <lucasreynes@gmail.com>
2018-09-20 18:56:19 +0200

    Over

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -3,6 +3,7 @@ import java.io.File
 import com.github.tototoshi.csv._
 import io.circe.generic.semiauto.deriveEncoder
 import io.circe.Encoder
+import io.circe.syntax._
 
 object Test extends App {
 
@@ -15,8 +16,7 @@ object Test extends App {
   val pisValues: Seq[(String, Seq[String])] = values.all.map(v => (v.head, v.tail))
   val correctValues: Seq[PhoneInformation] = pisValues.groupBy(_._1).map(a => PhoneInformation(a._1, a._2.flatMap(_._2))).toSeq
 
-  correctValues.asJon
-
+  println(correctValues.asJson)
 
 }
 
